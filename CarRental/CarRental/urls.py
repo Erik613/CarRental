2d8d107464e49c19c7d795b6e8cb09b42194f4aa
Erik613 <erikelges@gmail.com>
2021-04-27 14:33:36 +0200

    Grundlegende Loginfunktion

--- CarRental/CarRental/urls.py
+++ CarRental/CarRental/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('CarManagement.urls'))
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('CarManagement.urls')),
 ]
