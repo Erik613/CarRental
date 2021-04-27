5299e9dc4191b57b1dbb791eba79b9c1d712efe7
Patrick <patrickmatern1997@gmail.com>
2021-04-27 15:50:34 +0200

    new_customer methode added

--- CarRental/CarManagement/views.py
+++ CarRental/CarManagement/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from CarManagement.models import Car
 from CarManagement.models import Reservation
+from CarManagement.models import Customers
 
 # Create your views here.
 
@@ -18,7 +19,8 @@ def update(request): # möglichkeit jeden wert anzupassen
     return HttpResponse("Updated")
 
 def new_car(request):
-    car = Car(brand="BMW", model_name="A4", license_plate="BI-EE-1997", seats=4, km_age=250000, construction_date=2006, vehicle_type="SUV", gear_type="1")
+    car = Car(brand="BMW", model_name="A4", license_plate="BI-EE-1997", seats=4, km_age=250000, construction_date=2006,
+              vehicle_type="SUV", gear_type="1")
     car.save()
     return HttpResponse("Ok: ")
 
@@ -43,10 +45,15 @@ def car_set_km_age(request):
     return HttpResponse("Cars km_age changed")
 
 def new_reservation(requests):
-    reservation = Reservation(id_user="test", car_id="1", start_date=datetime.datetime.now(), end_date=datetime.datetime.now())
+    reservation = Reservation(id_user="test", car_id="1", start_date=datetime.datetime.now(),
+                              end_date=datetime.datetime.now())
     reservation.save()
     return HttpResponse("OK: ")
 
 def reservation_get_all(request):
     reservation = Reservation.objects.values()
     return HttpResponse(reservation)
+
+def new_customer(request):
+    customer = Customers(first_name="Patrick", sec_name="Matern", birth_date=(datetime.date(1997, 4, 9)),
+                         residence_street="Test Str", residence_number="69", residence_city="Bielefeld")
