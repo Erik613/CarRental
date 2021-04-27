4420591c3054ecb02d2adf556a99fedbff0112fa
Erik613 <erikelges@gmail.com>
2021-04-27 13:23:36 +0200

    First Tabels (Models) and Views

--- CarRental/CarManagement/models.py
+++ CarRental/CarManagement/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Car(models.Model):
+    brand = models.CharField(max_length=100)
+    model_name = models.CharField(max_length=250)
+    license_plate = models.CharField(max_length=50)
+    seats = models.IntegerField()
+    km_age = models.IntegerField()
+    construction_date = models.SmallIntegerField()
+    vehicle_type = models.CharField(max_length=100)     #SUV, Limosine, etc...
+    gear_type = models.CharField(max_length=50)
+    
+
+    
