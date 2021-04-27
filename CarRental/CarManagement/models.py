e1694479642cf816ba5dabe145c43336caf7ad84
Patrick <patrickmatern1997@gmail.com>
2021-04-27 15:43:45 +0200

    new table new table added

--- CarRental/CarManagement/models.py
+++ CarRental/CarManagement/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Car(models.Model):
     brand = models.CharField(max_length=100)
     model_name = models.CharField(max_length=250)
@@ -13,8 +14,19 @@ class Car(models.Model):
     gear_type = models.CharField(max_length=50)
     reserved = models.BooleanField(default=False)
 
+
 class Reservation(models.Model):
     id_user = models.CharField(max_length=50)
     car_id = models.IntegerField()
     start_date = models.DateField()
     end_date = models.DateField()
+
+
+class Customers(models.Model):
+    first_name = models.CharField(max_length=50)
+    sec_name = models.CharField(max_length=50)
+    birth_date = models.DateField()
+    residence_street = models.CharField(max_length=50)
+    residence_number = models.CharField(max_length=50)
+    residence_city = models.CharField(max_length=50)
+
