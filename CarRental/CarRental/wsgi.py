06c3d7308f26f83ad07f9fb9164d6a94d8f5381a
erik.elges <e.elges@obwyse.com>
2021-04-29 11:27:51 +0200

    Für TestDpl

--- CarRental/CarRental/wsgi.py
+++ CarRental/CarRental/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CarRental.settings')
+os.environ["DJANGO_SETTINGS_MODULE"] = "CarRental.settings"
+
+
 
 application = get_wsgi_application()
